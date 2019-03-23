2216c292b7d06e66645b3653c88eacb3f71c012d
Janusz Tomasik <januszveiro@gmail.com>
2019-03-23 14:13:20 +0100

    Refactor insights

--- src/components/pages/sections/InsightsSection.js
+++ src/components/pages/sections/InsightsSection.js
@@ -5,16 +5,22 @@ const InsightsSection = () => {
     return (
         <MDBCard className="cascading-admin-card">
             <MDBCardBody className="px-1">
-                <div className="insights-card">
-                    <div className="text-center pt-3">
-                        <h5>Live Insights</h5>
-                        <hr />
-                        <div className="businesshq-bg-light py-4 px-3 text-white m-2 rounded-lg"><small>23.03.2019: Connect your Google Analytics</small></div>
-                    </div>
+                <div className="insights-card text-center ">
+                    <h5>Live Insights</h5>
+                    <hr />
+                    <SingleInsight insightContent="Add bank account" />
+                    <SingleInsight insightContent={"Connect Google Analytics"}/>
                 </div>
             </MDBCardBody>
         </MDBCard>
     );
 }
 
+const SingleInsight = ({insightContent}) => {
+    return (
+        <div className="pt-2">
+            <div className="businesshq-bg-light py-4 px-3 text-white my-2 mx-3 rounded-lg"><small>{insightContent}</small></div>
+        </div>
+    )
+}
 export default InsightsSection;
